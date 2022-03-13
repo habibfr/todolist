05820a975b332d069c840460390a38eea7cbb198
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-13 12:29:29 +0700

    filter todo

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // event listener
 todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //functions
 function addTodo(event) {
@@ -62,3 +64,33 @@ function deleteCheck(event) {
     todo.classList.toggle("completed");
   }
 }
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+
+  todos.forEach((todo) => {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+
+
+
